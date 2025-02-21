1d4602c11b594e41104082b006045bec223b5c03
DQuintero <Ingdanilquintero9@gmail.com>
2025-02-21 18:35:05 -0500

    Cambios Swagger

--- LBAngularNet/LBAngularNet/Program.cs
+++ LBAngularNet/LBAngularNet/Program.cs
@@ -1,16 +1,10 @@
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-// Configurar URLs explícitamente
-//builder.WebHost.UseUrls("http://localhost:4000", "https://localhost:4001");
 
-
-// Add Swagger Config
 AddSwaggerConfig();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
+AddControllersViews();
+AddHttpDirections();
 
 // CORS
 builder.Services.AddCors(options =>
@@ -33,19 +27,9 @@ if (app.Environment.IsDevelopment())
 {
     // Habilita swagger en dllo
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
-        c.RoutePrefix = string.Empty; // Para acceder directamente en "/"
-    });
-    app.UseHsts();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-//app.MapGet("/hello", () => Results.Ok("Hola Mundo"))
-//   .WithName("GetHello")
-//   .WithOpenApi(); // Habilita documentación para este endpoint
 
 app.UseStaticFiles();
 app.UseRouting();
@@ -67,20 +51,21 @@ void AddSwaggerConfig()
 {
     // Agregar servicios de Swagger
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(options =>
+    builder.Services.AddSwaggerGen();
+}
+
+//
+void AddHttpDirections()
+{
+    builder.Services.AddHttpsRedirection(options =>
     {
-        options.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "Mi API con Controllers",
-            Version = "v1",
-            Description = "Ejemplo de API con Swagger en .NET 6+",
-            Contact = new OpenApiContact
-            {
-                Name = "Tu Nombre",
-                Email = "tu@email.com",
-                Url = new Uri("https://tusitio.com")
-            }
-        });
+        options.HttpsPort = 7145;
     });
 }
 
+
+void AddControllersViews()
+{
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+}
