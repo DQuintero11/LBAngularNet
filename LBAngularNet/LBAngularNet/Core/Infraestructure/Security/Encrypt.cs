07cfda4173bcf86ad38f3bf955a0872bf47373c5
DQuintero <Ingdanilquintero9@gmail.com>
2025-02-21 23:42:17 -0500

    Creacion BASE64 ENCODE DECODE

---
+++ LBAngularNet/LBAngularNet/Core/Infraestructure/Security/Encrypt.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace LBAngularNet.Core.Infraestructure.Security
+{
+    public class Encrypt
+    {
+        public string Codificar(string txt)
+        {
+            string base64Encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(txt));
+            return base64Encoded;
+        }
+
+        public string Decodificar (string txt)
+        {
+            string base64Decoded = Encoding.UTF8.GetString(Convert.FromBase64String(txt));
+            return base64Decoded;
+        }
+    }
+}
